d43385e77a58513ef60823c5b03d932d50087448
ZexTar <borisstodickg@gmail.com>
2018-09-03 12:40:31 +0200

    reindent code

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # face_recognition_api
 1.Clone this repo
+
 2.Run npm install
+
 3.Run npm start
+
+
 4.You must add your own API key in the face_recognition_app/src/App.js file to connect to Clarifai API.
+
 5.Grab Clarifai API key https://www.clarifai.com/
+
 ** Make sure you use postgreSQL instead of mySQL for this code base.
--- server.js
+++ server.js
@@ -5,13 +5,13 @@ const cors = require('cors');
 const knex = require('knex');
 
 const db = knex({
-  client: 'pg',
-  connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : '1',
-    database : 'smart-brain'
-  }
+	client: 'pg',
+	connection: {
+		host : '127.0.0.1',
+		user : 'postgres',
+		password : '1',
+		database : 'smart-brain'
+	}
 });
 
 db.select('*').from ('users').then (data => {
@@ -23,29 +23,29 @@ app.use(bodyParser.json());
 app.use(cors()); 
 
 app.get('/users/:id', (req,res) => {
-    const {id} = req.params;
-    db.select('*').from('users').where({id})
-    .then (user => {
-    	if (user.length){
-    		res.json(user[0])
-    	}
-    	else {res.status(400).json('not found')}
-    	})
+	const {id} = req.params;
+	db.select('*').from('users').where({id})
+	.then (user => {
+		if (user.length){
+			res.json(user[0])
+		}
+		else {res.status(400).json('not found')}
+	})
 })
 
 app.put('/picture', (req,res) => {
-    const {id} = req.body;
-    db('users').where('id', '=', id)
-    .increment('enteries', 1)
-    .returning('enteries')
-    .then(enteries => {
-        res.json(enteries[0]);
-    })
-    .catch(err => res.status(400).json('invalid request'));
- })
+	const {id} = req.body;
+	db('users').where('id', '=', id)
+	.increment('enteries', 1)
+	.returning('enteries')
+	.then(enteries => {
+		res.json(enteries[0]);
+	})
+	.catch(err => res.status(400).json('invalid request'));
+})
 
 app.get('/', (req,res)=>
-    res.send(database.users));
+	res.send(database.users));
 
 app.post('/signin', (req,res)=>{
 	const {email, password} = req.body;
@@ -60,18 +60,18 @@ app.post('/signin', (req,res)=>{
 		if (isValid){
 			console.log(isValid);
 			db.select('*')
-        	.from('users')
-        	.where('email', '=', req.body.email)
-        .then(user=>{
-            res.json(user[0])
-        })
-            .catch(err=> res.status(400).json('cannot find user'))
+			.from('users')
+			.where('email', '=', req.body.email)
+			.then(user=>{
+				res.json(user[0])
+			})
+			.catch(err=> res.status(400).json('cannot find user'))
 		}
 		else
 			res.status(400).json('purgeru')
 	})
 	.catch(err => res.status(400).json('invalid request'));
-	})
+})
 
 app.post('/register', (req,res)=> {
 	const {email, password, name} = req.body;
@@ -85,25 +85,25 @@ app.post('/register', (req,res)=> {
 			email:email,
 			hash:hash
 		})
-	 .into('login')
-	 .returning('email')
-	 .then(loginEmail=>{
-	 	return trx('users')
-	 	.returning('*')
-	 	.insert({
-	 		email:loginEmail[0],
-	 		name:name,
-    		joined:new Date()
-	 	})
-	 	})
-	.then (user => {
-    	res.json(user[0]);
-	})
-	.then(trx.commit)
-	.catch(trx.rollback)
-}
-)})
-    
+		.into('login')
+		.returning('email')
+		.then(loginEmail=>{
+			return trx('users')
+			.returning('*')
+			.insert({
+				email:loginEmail[0],
+				name:name,
+				joined:new Date()
+			})
+		})
+		.then (user => {
+			res.json(user[0]);
+		})
+		.then(trx.commit)
+		.catch(trx.rollback)
+	}
+	)})
+
 app.listen (3000,()=>{
-    console.log ('server is running on port 3000')
+	console.log ('server is running on port 3000')
 })
